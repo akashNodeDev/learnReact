a78c3f2858c180ad36d4e5ea7f66d57f922cd0f2
akash <akash.agrawal445@gmail.com>
2023-10-16 16:38:04 +0530

    correction done for the nested element push

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 /*
-Nested Structure
-<div id="parent">
-    <div id="child">
-        <h1> Nested Struction </h1>
-    </div>
-</div
-
+// Nested Structure
+// <div id="parent">
+//     <div id="child">
+//         <h1> Nested Struction </h1>
+//     </div>
+// </div>
+// React.createElelemnt(object)=> HTML Browser Understands(ReacctDOM converts the object in the HTML while rendering it on the browser)
 */
 
 
 const heading = React.createElement("div",
-                                {id:"Paarent"},
+                                {id:"parent"},
                                 React.createElement("div",{id:"child"},
                                 React.createElement("h1",{},"Hello from inside"))
                                 );
-console.log(parent);
+console.log(heading);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
