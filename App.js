4b3009b186ba18064f912de3be29eca7ce9b257f
akash <akash.agrawal445@gmail.com>
2023-10-16 11:46:59 +0530

    added the nested element through react

--- App.js
+++ App.js
@@ -1,3 +1,19 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello React");
+/*
+Nested Structure
+<div id="parent">
+    <div id="child">
+        <h1> Nested Struction </h1>
+    </div>
+</div
+
+*/
+
+
+const heading = React.createElement("div",
+                                {id:"Paarent"},
+                                React.createElement("div",{id:"child"},
+                                React.createElement("h1",{},"Hello from inside"))
+                                );
+console.log(parent);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
