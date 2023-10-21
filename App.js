3747b1c88a63ec4e01640fd077cc67097fe87983
akash <akash.agrawal445@gmail.com>
2023-10-21 14:11:10 +0530

    component composition

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (<h1 className="topCls">Hello Learning Node JS</h1>);
+const myNum = 120;
+const Title = ()=> (
+    <h1 className="head" tabIndex="5">
+        My First Functional Component for title
+        <p>{myNum}</p>
+    </h1>
+)
+const Test = () => (
+    <h3>Hello</h3>
+)
+
+const Heading =() => (
+    <div className="container">
+        <Title/>
+        {Test()}
+        <h2> Example of the component composition</h2>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<Heading/>);
 
 
