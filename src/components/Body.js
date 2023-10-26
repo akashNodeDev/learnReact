7c9c8d0a8739e9c516df01b75593d5944e13c40e
akash <akash.agrawal445@gmail.com>
2023-10-26 15:20:52 +0530

    useEffect hook introduction

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,25 @@
 import ResturantCard from "./ResturantCard";
-import resList from "../utils/mockData";
-import {useState} from "react";
+//import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [listOfResturants,setListOfRestrants] = useState(resList);
+  const [listOfResturants, setListOfRestrants] = useState([]);
+
+  // useEffect Introduction
+  useEffect(() => {
+    // console.log("UseEffectCallback function gets rendered");
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    let data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4490568&lng=78.3927348&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    //console.log("Data=",data);
+    const jsonData = await data.json();
+    //console.log("JSON DATA=",jsonData)
+    setListOfRestrants(jsonData.data.cards[5].card.card.gridElements.infoWithStyle.restaurants)
+  };
 
   return (
     <div className="body">
@@ -12,8 +28,10 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             // filter resturant
-            let filterData = listOfResturants.filter((res) => res.info.avgRating > 4 );
-            setListOfRestrants(filterData)
+            let filterData = listOfResturants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestrants(filterData);
           }}
         >
           Top Rated
