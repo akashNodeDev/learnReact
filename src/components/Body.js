43845c20acd1ddbc2b938e099d7f2ba15376fdd3
akash <akash.agrawal445@gmail.com>
2023-10-26 11:35:34 +0530

    dynamically use of the useEffect hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,131 +3,7 @@ import resList from "../utils/mockData";
 import {useState} from "react";
 
 const Body = () => {
-  const [listOfResturants,setListOfRestrants] = useState([
-    {
-      info: {
-        id: "632152",
-        name: "Soul Rasa",
-        cloudinaryImageId: "uggeyavij0hvhue0wmci",
-        costForTwo: "₹300 for two",
-        cuisines: [
-          "Indian",
-          "Healthy Food",
-          "Home Food",
-          "South Indian",
-          "North Indian",
-        ],
-        avgRating: 3.2,
-        feeDetails: {
-          restaurantId: "632152",
-          fees: [
-            {
-              name: "BASE_DISTANCE",
-              fee: 5100,
-            },
-            {
-              name: "BASE_TIME",
-            },
-            {
-              name: "ANCILLARY_SURGE_FEE",
-            },
-          ],
-          totalFee: 5100,
-        },
-        avgRatingString: "3.2",
-        sla: {
-          deliveryTime: 27,
-          lastMileTravel: 5,
-          serviceability: "SERVICEABLE",
-          slaString: "27 mins",
-          lastMileTravelString: "5.0 km",
-          iconType: "ICON_TYPE_EMPTY",
-        },
-      },
-    },
-    {
-      info: {
-        id: "632153",
-        name: "Dominoz",
-        cloudinaryImageId: "uggeyavij0hvhue0wmci",
-        costForTwo: "₹300 for two",
-        cuisines: [
-          "Indian",
-          "Healthy Food",
-          "Home Food",
-          "South Indian",
-          "North Indian",
-        ],
-        avgRating: 4.2,
-        feeDetails: {
-          restaurantId: "632152",
-          fees: [
-            {
-              name: "BASE_DISTANCE",
-              fee: 5100,
-            },
-            {
-              name: "BASE_TIME",
-            },
-            {
-              name: "ANCILLARY_SURGE_FEE",
-            },
-          ],
-          totalFee: 5100,
-        },
-        avgRatingString: "4.2",
-        sla: {
-          deliveryTime: 27,
-          lastMileTravel: 5,
-          serviceability: "SERVICEABLE",
-          slaString: "27 mins",
-          lastMileTravelString: "5.0 km",
-          iconType: "ICON_TYPE_EMPTY",
-        },
-      },
-    },
-    {
-      info: {
-        id: "632154",
-        name: "KFC",
-        cloudinaryImageId: "uggeyavij0hvhue0wmci",
-        costForTwo: "₹300 for two",
-        cuisines: [
-          "Indian",
-          "Healthy Food",
-          "Home Food",
-          "South Indian",
-          "North Indian",
-        ],
-        avgRating: 4.2,
-        feeDetails: {
-          restaurantId: "632152",
-          fees: [
-            {
-              name: "BASE_DISTANCE",
-              fee: 5100,
-            },
-            {
-              name: "BASE_TIME",
-            },
-            {
-              name: "ANCILLARY_SURGE_FEE",
-            },
-          ],
-          totalFee: 5100,
-        },
-        avgRatingString: "4.2",
-        sla: {
-          deliveryTime: 27,
-          lastMileTravel: 5,
-          serviceability: "SERVICEABLE",
-          slaString: "27 mins",
-          lastMileTravelString: "5.0 km",
-          iconType: "ICON_TYPE_EMPTY",
-        },
-      },
-    },
-  ]);
+  const [listOfResturants,setListOfRestrants] = useState(resList);
 
   return (
     <div className="body">
@@ -135,10 +11,8 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            // console.log("Button Clicked");
             // filter resturant
             let filterData = listOfResturants.filter((res) => res.info.avgRating > 4 );
-            //console.log("Data=",filterData)
             setListOfRestrants(filterData)
           }}
         >
